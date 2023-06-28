3a05872add53084d7536cae2f550b3a02f2b3b5c
msorvoja <mika@gispo.fi>
2023-06-28 07:37:32 +0300

    Add DBSCAN from toolkit to plugin

---
+++ eis_qgis_plugin/processing/algorithms/dbscan.py
@@ -0,0 +1,46 @@
+from qgis.core import (
+    QgsProcessingParameterFolderDestination,
+    QgsProcessingParameterNumber,
+    QgsProcessingParameterRasterLayer,
+)
+
+from eis_qgis_plugin.processing.eis_processing_algorithm import EISProcessingAlgorithm
+
+
+class EISDbscan(EISProcessingAlgorithm):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._name = "dbscan"
+        self._display_name = "DBSCAN"
+        self._group = "Exploratory analysis"
+        self._group_id = "exploratory_analysis"
+        self._short_help_string = "Perform DBSCAN"
+
+    def initAlgorithm(self, config=None):
+        
+        self.alg_parameters = ["input_raster", "max_distance", "min_samples", "output_folder"]
+
+        self.addParameter(
+            QgsProcessingParameterRasterLayer(
+                name=self.alg_parameters[0], description="Input raster"
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterNumber(
+                name=self.alg_parameters[1], description="Maximum distance"
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterNumber(
+                name=self.alg_parameters[2], description="Minimum number of samples"
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterFolderDestination(
+                name=self.alg_parameters[3], description="Output folder"
+            )
+        )
