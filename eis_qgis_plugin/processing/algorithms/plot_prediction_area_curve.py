a566c9f5f2c5f9a9d7a20fac0a69e4cc7a9691e3
msorvoja <mika@gispo.fi>
2023-07-27 13:23:31 +0300

    Add plot_prediction_area_curve

---
+++ eis_qgis_plugin/processing/algorithms/plot_prediction_area_curve.py
@@ -0,0 +1,50 @@
+from qgis.core import (
+    QgsProcessingParameterMatrix,
+    QgsProcessingParameterFileDestination,    
+)
+
+from eis_qgis_plugin.processing.eis_processing_algorithm import EISProcessingAlgorithm
+
+
+class EISPlotPredictionAreaCurve(EISProcessingAlgorithm):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._name = "plot_prediction_area_curve"
+        self._display_name = "Plot prediction area curve"
+        self._group = "Validation"
+        self._group_id = "validation"
+        self._short_help_string = "Plot prediction-area (P-A) curve"
+
+    def initAlgorithm(self, config=None):
+        
+        self.alg_parameters = [
+            "true_positive_rate_values",
+            "proportion_of_area_values",
+            "threshold_values",
+            "plot_figure"]
+
+        self.addParameter(
+            QgsProcessingParameterMatrix(
+                name=self.alg_parameters[0], description="True positive rate values"
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterMatrix(
+                name=self.alg_parameters[1], description="Proportion of area values"
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterMatrix(
+                name=self.alg_parameters[2], description="Threshold values"
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterFileDestination(
+                name=self.alg_parameters[3], description="Output plot figure"
+            )
+        )
