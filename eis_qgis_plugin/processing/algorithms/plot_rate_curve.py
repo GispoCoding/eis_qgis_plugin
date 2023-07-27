f31311d985bf38e76ed35c54e1987d64d7f94d12
msorvoja <mika@gispo.fi>
2023-07-27 13:23:50 +0300

    Add plot_rate_curve

---
+++ eis_qgis_plugin/processing/algorithms/plot_rate_curve.py
@@ -0,0 +1,47 @@
+from qgis.core import (
+    QgsProcessingParameterFileDestination,
+    QgsProcessingParameterMatrix,
+    QgsProcessingParameterString,
+)
+
+from eis_qgis_plugin.processing.eis_processing_algorithm import EISProcessingAlgorithm
+
+
+class EISPlotRateCurve(EISProcessingAlgorithm):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._name = "plot_rate_curve"
+        self._display_name = "Plot rate curve"
+        self._group = "Validation"
+        self._group_id = "validation"
+        self._short_help_string = "Plot success rate, prediction rate or ROC curve"
+
+    def initAlgorithm(self, config=None):
+        
+        self.alg_parameters = ["x_values", "y_values", "plot_type", "plot_figure"]
+
+        self.addParameter(
+            QgsProcessingParameterMatrix(
+                name=self.alg_parameters[0], description="False positive rate or proportion of area"
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterMatrix(
+                name=self.alg_parameters[1], description="True positive rate"
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterString(
+                name=self.alg_parameters[2], description="Plot type"
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterFileDestination(
+                name=self.alg_parameters[3], description="Output plot figure"
+            )
+        )
