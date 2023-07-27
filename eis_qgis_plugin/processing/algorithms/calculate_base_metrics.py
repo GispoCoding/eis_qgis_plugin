2170afab8681dacda679115bf18e7396b61a5286
msorvoja <mika@gispo.fi>
2023-07-27 13:21:19 +0300

    Add calculate_base_metrics

---
+++ eis_qgis_plugin/processing/algorithms/calculate_base_metrics.py
@@ -0,0 +1,54 @@
+from qgis.core import (
+    QgsProcessingParameterFileDestination,
+    QgsProcessingParameterNumber,
+    QgsProcessingParameterRasterLayer,
+    QgsProcessingParameterVectorLayer
+)
+
+from eis_qgis_plugin.processing.eis_processing_algorithm import EISProcessingAlgorithm
+
+
+class EISCalculateBaseMetrics(EISProcessingAlgorithm):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._name = "calculate_base_metrics"
+        self._display_name = "Calculate base metrics"
+        self._group = "Validation"
+        self._group_id = "validation"
+        self._display_name = "Calculate base metrics"
+
+    def initAlgorithm(self, config=None):
+        
+        self.alg_parameters = ["input_raster", "input_deposits", "band", "negatives", "output_metrics"]
+
+        self.addParameter(
+            QgsProcessingParameterRasterLayer(
+                name=self.alg_parameters[0], description="Input raster"
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterVectorLayer(
+                name=self.alg_parameters[1], description="Mineral deposits"
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterNumber(
+                name=self.alg_parameters[2], description="Band index"
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterVectorLayer(
+                name=self.alg_parameters[3], description="Negative locations"
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterFileDestination(
+                name=self.alg_parameters[4], description="Output metrics"
+            )
+        )
