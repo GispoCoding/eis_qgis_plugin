b45fef6c677857a84c31d114405b2f647af0f951
Niko Aarnio <niko.aarnio@protonmail.com>
2023-11-20 13:53:56 +0200

    Added new prototype version of modeling wizard

--- eis_qgis_plugin/wizard/model/model_wizard.py
+++ eis_qgis_plugin/wizard/model/model_wizard.py
@@ -1,15 +1,13 @@
 from qgis.PyQt.QtWidgets import QDialog, QWizard, QTableWidget
 
-from qgis.core import QgsProject, QgsLayerTreeModel
 
-from qgis.gui import QgsLayerTreeView
 
 from eis_qgis_plugin.qgis_plugin_tools.tools.resources import load_ui
 
 # from eis_qgis_plugin.wizard.explore.wizard_explore import EISWizardExplore
 # from eis_qgis_plugin.wizard.preprocess.wizard_preprocess import EISWizardPreprocess
 
-FORM_CLASS: QDialog = load_ui("model/post_modeling.ui")
+FORM_CLASS: QDialog = load_ui("model/model_view.ui")
 
 
 class EISWizardModeling(QWizard, FORM_CLASS):
@@ -26,8 +24,9 @@ class EISWizardModeling(QWizard, FORM_CLASS):
 
         # self.layer_tree = QgsLayerTreeView()
 
-        root = QgsProject.instance().layerTreeRoot()
-        model = QgsLayerTreeModel(root)
-        view = QgsLayerTreeView()
-        view.setModel(model)
-        view.show()
+        # WORKING?
+        # root = QgsProject.instance().layerTreeRoot()
+        # model = QgsLayerTreeModel(root)
+        # view = QgsLayerTreeView()
+        # view.setModel(model)
+        # view.show()
