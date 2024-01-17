04037cb3bffe86d0a43ad3cb392eb2abb88e6772
Niko Aarnio <niko.aarnio@protonmail.com>
2024-01-17 13:20:51 +0200

    Add/fix unify_rasters alg

--- eis_qgis_plugin/processing/algorithms/raster_processing/unify_rasters.py
+++ eis_qgis_plugin/processing/algorithms/raster_processing/unify_rasters.py
@@ -1,5 +1,9 @@
 from qgis.core import (
-    QgsProcessingParameterRasterDestination,
+    QgsProcessing,
+    QgsProcessingParameterBoolean,
+    QgsProcessingParameterEnum,
+    QgsProcessingParameterFolderDestination,
+    QgsProcessingParameterMultipleLayers,
     QgsProcessingParameterRasterLayer,
 )
 
@@ -17,7 +21,13 @@ class EISUnifyRasters(EISProcessingAlgorithm):
         self._short_help_string = "Unify a set of rasters"
 
     def initAlgorithm(self, config=None):
-        self.alg_parameters = ["base_raster", "rasters", "output_raster"]
+        self.alg_parameters = [
+            "base_raster",
+            "rasters_to_unify",
+            "resampling_method",
+            "same_extent",
+            "output_directory"
+        ]
 
         self.addParameter(
             QgsProcessingParameterRasterLayer(
@@ -26,13 +36,36 @@ class EISUnifyRasters(EISProcessingAlgorithm):
         )
 
         self.addParameter(
-            QgsProcessingParameterRasterLayer(
-                name=self.alg_parameters[1], description="Raster(s) to unify"
+            QgsProcessingParameterMultipleLayers(
+                name=self.alg_parameters[1], description="Rasters to unify", layerType=QgsProcessing.TypeRaster
             )
         )
 
         self.addParameter(
-            QgsProcessingParameterRasterDestination(
-                name=self.alg_parameters[2], description="Output raster"
+            QgsProcessingParameterEnum(
+                name=self.alg_parameters[2],
+                description="Resampling method",
+                options=[
+                    "Nearest",
+                    "Bilinear",
+                    "Cubic",
+                    "Average",
+                    "Gauss",
+                    "Max",
+                    "Min",
+                ],
+                defaultValue="Nearest",
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterBoolean(
+                name=self.alg_parameters[3], description="Same extent"
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterFolderDestination(
+                name=self.alg_parameters[4], description="Output directory"
             )
         )
