c32a13a7501f30f3e1db84dbf7f173dc2fe137f7
Niko Aarnio <niko.aarnio@protonmail.com>
2024-01-30 13:14:01 +0200

    Add initialization for different tabs (draft)

--- eis_qgis_plugin/wizard/wizard_modeling.py
+++ eis_qgis_plugin/wizard/wizard_modeling.py
@@ -1,8 +1,10 @@
 from qgis.PyQt.QtWidgets import (
     QComboBox,
     QDialog,
+    QGroupBox,
     QProgressBar,
     QPushButton,
+    QRadioButton,
     QSizePolicy,
     QStackedWidget,
     QTabWidget,
@@ -13,10 +15,11 @@ from qgis.PyQt.QtWidgets import (
 from eis_qgis_plugin.qgis_plugin_tools.tools.resources import load_ui
 from eis_qgis_plugin.wizard.modeling.gradient_boosting import EISWizardGradientBoosting
 from eis_qgis_plugin.wizard.modeling.logistic_regression import EISWizardLogisticRegression
+from eis_qgis_plugin.wizard.modeling.model_data_table import ModelDataTable
 from eis_qgis_plugin.wizard.modeling.model_template import ModelType
 from eis_qgis_plugin.wizard.modeling.random_forests import EISWizardRandomForests
 
-FORM_CLASS: QDialog = load_ui("model/wizard_model_new.ui")
+FORM_CLASS: QDialog = load_ui("model/wizard_model_new_2.ui")
 
 
 class EISWizardModeling(QWidget, FORM_CLASS):
@@ -31,12 +34,26 @@ class EISWizardModeling(QWidget, FORM_CLASS):
     training_log: QTextEdit
     training_progress_bar: QProgressBar
 
+    # Application
+    application_data_groupbox: QGroupBox
+    table_placeholder: QWidget
+    test_radiobutton: QRadioButton
+    predict_radiobutton: QRadioButton
+
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
         self.setupUi(self)
 
         self.model_selection.currentIndexChanged['int'].connect(self.model_parameters_container.setCurrentIndex)
 
+        self.initialize_model_data_preparation()
+        self.initialize_model_creation()
+        self.initialize_model_application()
+
+    def initialize_model_data_preparation(self):
+        pass
+
+    def initialize_model_creation(self):
         self.model_parameters_container.currentChanged.connect(self.resize_parameter_container)
 
         self.start_training_btn.clicked.connect(self.train_selected_model)
@@ -55,7 +72,6 @@ class EISWizardModeling(QWidget, FORM_CLASS):
 
         self.resize_parameter_container(0)
 
-        print(self.model_parameters_container.height())
         # headers = ["Precision", "Recall", "Accuracy", "Support"]
         # self.table.setHorizontalHeaderLabels(headers)
         # self.table.setDisabled(True)
@@ -71,6 +87,25 @@ class EISWizardModeling(QWidget, FORM_CLASS):
         # view.show()
 
         # Create pages for parameters
+
+    def initialize_model_application(self):
+        table = ModelDataTable(self)
+        self.application_data_table_layout.addWidget(table)
+
+        self.test_radiobutton.toggled.connect(lambda: self.application_mode_changed(self.test_radiobutton))
+        self.predict_radiobutton.toggled.connect(lambda: self.application_mode_changed(self.predict_radiobutton))
+
+
+    def application_mode_changed(self, btn):
+        if self.test_radiobutton.isChecked():
+            self.application_y_label.show()
+            self.application_y.show()
+            self.evaluation_metrics_groupbox.show()
+        else:
+            self.application_y_label.hide()
+            self.application_y.hide()
+            self.evaluation_metrics_groupbox.hide()
+
 
     def train_selected_model(self):
         """Call the run method of the selected model."""
