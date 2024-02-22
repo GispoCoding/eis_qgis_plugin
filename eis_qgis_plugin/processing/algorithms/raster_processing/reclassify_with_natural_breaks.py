1138395dac53f707b36bb0631f96f5efaa329909
lehtonenp <115632450+lehtonenp@users.noreply.github.com>
2024-02-22 13:41:26 +0200

    Add Natural breaks tool

---
+++ eis_qgis_plugin/processing/algorithms/raster_processing/reclassify_with_natural_breaks.py
@@ -0,0 +1,46 @@
+from qgis.core import (
+    QgsProcessingParameterRasterDestination,
+    QgsProcessingParameterRasterLayer,
+    QgsProcessingParameterString,
+)
+
+from eis_qgis_plugin.processing.eis_processing_algorithm import EISProcessingAlgorithm
+
+
+class EISReclassifyWithNaturalBreaks(EISProcessingAlgorithm):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._name = "reclassify_with_natural_breaks"
+        self._display_name = "Reclassify with natural breaks"
+        self._group = "Raster Processing"
+        self._group_id = "raster_processing"
+        self._short_help_string = (
+            "Reclassify raster data set with natural breaks."
+        )
+
+    def initAlgorithm(self, config=None):
+        self.alg_parameters = [
+            "input_raster",
+            "number_of_classes",
+            "output_raster",
+        ]
+
+        input_raster_param = QgsProcessingParameterRasterLayer(
+            name=self.alg_parameters[0], description="Input raster"
+        )
+        input_raster_param.setHelp("The input raster data set.")
+        self.addParameter(input_raster_param)
+
+        breaks_param = QgsProcessingParameterString(
+            name=self.alg_parameters[1],
+            description="The number of classes"
+        )
+        breaks_param.setHelp("The number of classes for Natural breaks.")
+        self.addParameter(breaks_param)
+
+        output_raster_param = QgsProcessingParameterRasterDestination(
+            name=self.alg_parameters[2], description="Output raster"
+        )
+        output_raster_param.setHelp("The output raster data set.")
+        self.addParameter(output_raster_param)
