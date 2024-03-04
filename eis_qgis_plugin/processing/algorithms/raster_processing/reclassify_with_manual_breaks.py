26401f96e984fe2e864d0be855bafb40058797d6
Niko Aarnio <niko.aarnio@protonmail.com>
2024-03-04 15:46:07 +0200

    Implemented special handling for breaks parameter

--- eis_qgis_plugin/processing/algorithms/raster_processing/reclassify_with_manual_breaks.py
+++ eis_qgis_plugin/processing/algorithms/raster_processing/reclassify_with_manual_breaks.py
@@ -1,11 +1,18 @@
+import re
+from typing import Any, Dict, Optional
+
 from qgis.core import (
+    QgsProcessingContext,
+    QgsProcessingFeedback,
     QgsProcessingParameterBand,
+    QgsProcessingParameterDefinition,
     QgsProcessingParameterRasterDestination,
     QgsProcessingParameterRasterLayer,
     QgsProcessingParameterString,
 )
 
 from eis_qgis_plugin.processing.eis_processing_algorithm import EISProcessingAlgorithm
+from eis_qgis_plugin.processing.eis_toolkit_invoker import EISToolkitInvoker
 
 
 class EISReclassifyWithManualBreaks(EISProcessingAlgorithm):
@@ -57,3 +64,35 @@ class EISReclassifyWithManualBreaks(EISProcessingAlgorithm):
         )
         output_raster_param.setHelp("The output raster data set.")
         self.addParameter(output_raster_param)
+
+
+    def processAlgorithm(
+        self,
+        parameters: Dict[str, QgsProcessingParameterDefinition],
+        context: QgsProcessingContext,
+        feedback: Optional[QgsProcessingFeedback]
+    ) -> Dict[str, Any]:
+        if feedback is None:
+            feedback = QgsProcessingFeedback()
+        
+        breaks_param_i = 1
+        # Handle the manual breaks
+        breaks_raw = self.parameterAsString(parameters, self.alg_parameters[breaks_param_i], context).lower()
+        values = re.split(';|,', breaks_raw)
+        break_options = []
+        for value in values:
+            break_options.append("--" + self.alg_parameters[breaks_param_i].replace("_", "-"))
+            break_options.append(value) 
+
+        # Remove breaks from the list to not prepare them again in the next step
+        self.alg_parameters.pop(breaks_param_i)
+        typer_args, typer_options = self.prepare_arguments(parameters, context)
+        typer_options += break_options  # Combine lists
+
+        toolkit_invoker = EISToolkitInvoker()
+        toolkit_invoker.assemble_cli_command(self.name(), typer_args, typer_options)
+        results = toolkit_invoker.run_toolkit_command(feedback)
+
+        self.get_results(results, parameters)
+
+        return results
