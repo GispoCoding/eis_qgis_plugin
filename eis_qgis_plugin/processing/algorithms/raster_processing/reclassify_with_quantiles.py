63c2c2392daf79bfb212167ed83dd581f996502b
lehtonenp <115632450+lehtonenp@users.noreply.github.com>
2024-03-05 09:34:52 +0200

    Change referece to 'bands' to 'band'

--- eis_qgis_plugin/processing/algorithms/raster_processing/reclassify_with_quantiles.py
+++ eis_qgis_plugin/processing/algorithms/raster_processing/reclassify_with_quantiles.py
@@ -44,10 +44,10 @@ class EISReclassifyWithQuantiles(EISProcessingAlgorithm):
 
         bands_param = QgsProcessingParameterBand(
             name=self.alg_parameters[2],
-            description="Raster bands",
+            description="Raster band",
             parentLayerParameterName=self.alg_parameters[0],
         )
-        bands_param.setHelp("The bands to be reclassified.")
+        bands_param.setHelp("The band to be reclassified.")
         self.addParameter(bands_param)
 
         output_raster_param = QgsProcessingParameterRasterDestination(
