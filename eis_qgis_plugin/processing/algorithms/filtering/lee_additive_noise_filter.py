c75bc3b5969fec5148d8f06f2333c1fbac9ca200
lehtonenp <115632450+lehtonenp@users.noreply.github.com>
2024-03-07 14:56:56 +0200

    Add Lee Additive Noise filter function to plugin

---
+++ eis_qgis_plugin/processing/algorithms/filtering/lee_additive_noise_filter.py
@@ -0,0 +1,63 @@
+from eis_qgis_plugin.processing.eis_processing_algorithm import EISProcessingAlgorithm
+
+from qgis.core import (
+    QgsProcessingParameterNumber,
+    QgsProcessingParameterRasterDestination,
+    QgsProcessingParameterRasterLayer,
+)
+
+
+
+class EISLeeAdditiveNoiseFilter(EISProcessingAlgorithm):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._name = "lee_additive_noise_filter"
+        self._display_name = "Lee Additive Noise Filter"
+        self._group = "Filtering"
+        self._group_id = "filtering"
+        self._short_help_string = "Apply a Lee filter considering additive noise components to the input raster"
+
+    def initAlgorithm(self, config=None):
+        self.alg_parameters = [
+            "input_raster",
+            "size",
+            "add_noise_var",
+            "output_raster",
+        ]
+
+        input_raster_param = QgsProcessingParameterRasterLayer(
+                name=self.alg_parameters[0], description="Input raster"
+            )
+        input_raster_param.setHelp("The input raster data set.")
+        self.addParameter(input_raster_param)
+
+        noise_var_param = QgsProcessingParameterNumber(
+            name=self.alg_parameters[1],
+            description="Size",
+            minValue=1,
+            defaultValue=3,
+        )
+        noise_var_param.setHelp(
+            '''
+            The size of the filter window.
+            E.g., 3 means a 3x3 window. Default to 3.
+            '''
+        )
+        self.addParameter(noise_var_param)
+
+        window_size_param = QgsProcessingParameterNumber(
+            name=self.alg_parameters[2],
+            description="Additive Noise Variation",
+            minValue=0.1,
+            defaultValue=0.25,
+            type=QgsProcessingParameterNumber.Double,
+        )
+        window_size_param.setHelp("The additive noise variation. Default to 0.25.")
+        self.addParameter(window_size_param)
+
+        output_raster_param = QgsProcessingParameterRasterDestination(
+            name=self.alg_parameters[3], description="Output Raster"
+        )
+        output_raster_param.setHelp("The output raster data set.")
+        self.addParameter(output_raster_param)
