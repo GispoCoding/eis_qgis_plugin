69add4707ff4845214620d6712bd985d43ea06e9
Niko Aarnio <niko.aarnio@protonmail.com>
2024-03-12 16:30:03 +0200

    Fixed resample raster and added documentation

--- eis_qgis_plugin/processing/algorithms/raster_processing/resample_raster.py
+++ eis_qgis_plugin/processing/algorithms/raster_processing/resample_raster.py
@@ -16,58 +16,49 @@ class EISResampleRaster(EISProcessingAlgorithm):
         self._display_name = "Resample raster"
         self._group = "Raster Processing"
         self._group_id = "raster_processing"
-        self._short_help_string = "Resample raster to a new resolution"
+        self._short_help_string = "Resample raster to a new resolution."
 
     def initAlgorithm(self, config=None):
         self.alg_parameters = [
             "input_raster",
             "resolution",
             "resampling_method",
-            "target_resolution",
             "output_raster",
         ]
 
-        self.addParameter(
-            QgsProcessingParameterRasterLayer(
-                name=self.alg_parameters[0], description="Input raster"
-            )
+        input_raster_param = QgsProcessingParameterRasterLayer(
+            name=self.alg_parameters[0], description="Input raster"
         )
+        input_raster_param.setHelp("Input raster to be resampled.")
+        self.addParameter(input_raster_param)
 
-        self.addParameter(
-            QgsProcessingParameterNumber(
-                name=self.alg_parameters[1],
-                description="Resolution",
-                type=QgsProcessingParameterNumber.Double,
-            )
+        target_pixel_size_param = QgsProcessingParameterNumber(
+            name=self.alg_parameters[1],
+            description="Target pixel size",
+            type=QgsProcessingParameterNumber.Double,
         )
+        target_pixel_size_param.setHelp("The pixel size / resolution in the output raster.")
+        self.addParameter(target_pixel_size_param)
 
-        self.addParameter(
-            QgsProcessingParameterEnum(
-                name=self.alg_parameters[2],
-                description="Resampling method",
-                options=[
-                    "Nearest",
-                    "Bilinear",
-                    "Cubic",
-                    "Average",
-                    "Gauss",
-                    "Max",
-                    "Min",
-                ],
-                defaultValue="Nearest",
-            )
+        resampling_method_param = QgsProcessingParameterEnum(
+            name=self.alg_parameters[2],
+            description="Resampling method",
+            options=[
+                "Nearest",
+                "Bilinear",
+                "Cubic",
+                "Average",
+                "Gauss",
+                "Max",
+                "Min",
+            ],
+            defaultValue="Nearest",
         )
+        resampling_method_param.setHelp("The resampling method.")
+        self.addParameter(resampling_method_param)
 
-        self.addParameter(
-            QgsProcessingParameterNumber(
-                name=self.alg_parameters[2],
-                description="Target resolution",
-                type=QgsProcessingParameterNumber.Double,
-            )
+        output_raster_param = QgsProcessingParameterRasterDestination(
+            name=self.alg_parameters[3], description="Output raster"
         )
-
-        self.addParameter(
-            QgsProcessingParameterRasterDestination(
-                name=self.alg_parameters[3], description="Output raster"
-            )
-        )
+        output_raster_param.setHelp("Output resampled raster.")
+        self.addParameter(output_raster_param)
