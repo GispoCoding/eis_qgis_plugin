8c7645e3a6c5a3a229476b28a1e810bc16f9364d
Niko Aarnio <niko.aarnio@protonmail.com>
2024-04-17 13:37:48 +0300

    Added functionality to model data preparation tab. Very initial implementation, only button links to processing algs

--- eis_qgis_plugin/wizard/modeling/machine_learning/data_preparation.py
+++ eis_qgis_plugin/wizard/modeling/machine_learning/data_preparation.py
@@ -1,3 +1,4 @@
+import processing
 from qgis.PyQt.QtWidgets import QWidget
 
 from eis_qgis_plugin.qgis_plugin_tools.tools.resources import load_ui
@@ -12,3 +13,32 @@ class EISMLModelDataPreparation(QWidget, FORM_CLASS):
         self.setupUi(self)
 
         self.model_main = model_main
+
+        self.open_resample_btn.clicked.connect(lambda _: processing.execAlgorithmDialog('eis:resample_raster'))
+        self.open_reproject_btn.clicked.connect(lambda _: processing.execAlgorithmDialog('eis:reproject_raster'))
+        self.open_unify_btn.clicked.connect(lambda _: processing.execAlgorithmDialog('eis:unify_rasters'))
+        self.open_binarize_btn.clicked.connect(lambda _: processing.execAlgorithmDialog('eis:binarize'))
+        self.open_min_max_scale_btn.clicked.connect(lambda _: processing.execAlgorithmDialog('eis:min_max_scaling'))
+        self.open_z_score_normalize_btn.clicked.connect(
+            lambda _: processing.execAlgorithmDialog('eis:z_score_normalization')
+        )
+        self.open_winsorize_btn.clicked.connect(lambda _: processing.execAlgorithmDialog('eis:winsorize_transform'))
+        self.open_sigmoid_btn.clicked.connect(lambda _: processing.execAlgorithmDialog('eis:sigmoid_transform'))
+        self.open_logarithmic_btn.clicked.connect(lambda _: processing.execAlgorithmDialog('eis:log_transform'))
+        # self.open_one_hot_encode_btn.clicked.connect(
+            # lambda _: processing.execAlgorithmDialog('eis:one_hot_encode')
+        # ) #TODO
+        self.open_alr_btn.clicked.connect(lambda _: processing.execAlgorithmDialog('eis:alr_transform'))
+        self.open_inverse_alr_btn.clicked.connect(
+            lambda _: processing.execAlgorithmDialog('eis:inverse_alr_transform')
+        )
+        self.open_clr_btn.clicked.connect(lambda _: processing.execAlgorithmDialog('eis:clr_transform'))
+        self.open_inverse_clr_btn.clicked.connect(
+            lambda _: processing.execAlgorithmDialog('eis:inverse_clr_transform')
+        )
+        self.open_pairwise_logratio_btn.clicked.connect(
+            lambda _: processing.execAlgorithmDialog('eis:pairwise_logratio')
+        )
+        self.open_single_plr_btn.clicked.connect(lambda _: processing.execAlgorithmDialog('eis:single_plr_transform'))
+        self.open_single_ilr_btn.clicked.connect(lambda _: processing.execAlgorithmDialog('eis:single_ilr_transform'))
+        # self.balance_data_btn.clicked.connect(lambda _: processing.execAlgorithmDialog('eis:balance_data'))
