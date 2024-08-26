e133406e8a2d3fa12ed06e2eb6189c116265864d
Niko Aarnio <niko.aarnio@protonmail.com>
2024-08-26 15:23:19 +0300

    feat(wizard): implement training for MLP models in wizard

--- eis_qgis_plugin/wizard/modeling/machine_learning/models/mlp.py
+++ eis_qgis_plugin/wizard/modeling/machine_learning/models/mlp.py
@@ -53,12 +53,12 @@ class EISWizardMLP(EISMLModel):
         self.last_activation = QComboBox()
         self.training_tab.add_parameter_row(self.last_activation_label, self.last_activation)
 
-        self.epcohs_label = QLabel()
-        self.epcohs_label.setText("Epochs")
-        self.epcohs = QgsSpinBox()
-        self.epcohs.setMinimum(1)
-        self.epcohs.setValue(50)
-        self.training_tab.add_parameter_row(self.epcohs_label, self.epcohs)
+        self.epochs_label = QLabel()
+        self.epochs_label.setText("Epochs")
+        self.epochs = QgsSpinBox()
+        self.epochs.setMinimum(1)
+        self.epochs.setValue(50)
+        self.training_tab.add_parameter_row(self.epochs_label, self.epochs)
 
         self.batch_size_label = QLabel()
         self.batch_size_label.setText("Batch size")
@@ -91,6 +91,7 @@ class EISWizardMLP(EISMLModel):
         self.dropout_rate = QgsDoubleSpinBox()
         self.dropout_rate.setMinimum(0.0)
         self.dropout_rate.setMaximum(1.0)
+        self.dropout_rate.setValue(0.0)
         self.dropout_rate.setDecimals(5)
         self.training_tab.add_parameter_row(self.dropout_rate_label, self.dropout_rate)
 
@@ -115,9 +116,10 @@ class EISWizardMLP(EISMLModel):
         self.last_activation.addItems(["sigmoid", "softmax"])
         self.loss_function.addItems(["binary_crossentropy", "categorical_crossentropy"])
         
-        # Customize validation metrics for MLP classifier (no R2 for now)
+        # Customized validation metrics for MLP classifier (no R2 for now)
         self.training_tab.validation_metrics.clear()
-        self.training_tab.validation_metrics.addItems(["accuracy", "precision", "recall"])
+        self.training_tab.validation_metrics.addItems(["Accuracy", "Precision", "Recall"])
+        self.training_tab.validation_metrics.setCheckedItems(["Accuracy"])
 
 
     def initialize_regressor(self):
@@ -130,43 +132,88 @@ class EISWizardMLP(EISMLModel):
 
     def get_parameter_values(self, as_str: bool = False) -> Dict[str, Any]:
         return {
-            'n_estimators': self.n_estimators.value(),
-            'criterion': self.criterion.currentText() if as_str else self.criterion.currentIndex(),
-            'max_depth': self.max_depth.value()
+            "neurons": self.neurons.text(),
+            "output_neurons": self.output_neurons.value(),
+            "activation": self.activation.currentText() if as_str else self.activation.currentIndex(),
+            "last_activation": self.last_activation.currentText() if as_str else self.last_activation.currentIndex(),
+            "epochs": self.epochs.value(),
+            "batch_size": self.batch_size.value(),
+            "optimizer": self.optimizer.currentText() if as_str else self.optimizer.currentIndex(),
+            "learning_rate": self.learning_rate.value(),
+            "loss_function": self.loss_function.currentText() if as_str else self.loss_function.currentIndex(),
+            "dropout_rate": self.dropout_rate.value(),
+            "early_stopping": self.early_stopping.isChecked(),
+            "es_patience": self.es_patience.value()
         }
 
 
     def reset_parameters(self):
-        """Reset random forest parameters to defaults."""
+        """Reset MLP parameters to defaults."""
         self.training_tab.reset_parameters()
-
-        #TODO
-        pass
-        # self.n_estimators.setValue(100)
-        # self.criterion.setCurrentIndex(0)
-        # self.max_depth.setValue(-1)
+        
+        self.neurons.clear()
+        self.output_neurons.setValue(1)
+        self.activation.setCurrentIndex(0)
+        self.last_activation.setCurrentIndex(0)
+        self.epochs.setValue(50)
+        self.batch_size.setValue(32)
+        self.optimizer.setCurrentIndex(0)
+        self.learning_rate.setValue(0.001)
+        self.loss_function.setCurrentIndex(0)
+        self.dropout_rate.setValue(0.0)
+        self.early_stopping.setChecked(True)
+        self.es_patience.setValue(5)
 
 
     def set_tooltips(self):
         """Set tooltips for random forest parameters."""
         self.training_tab.set_tooltips()
 
-        #TODO
-        pass
+        neurons_tip = "Number of neurons in each hidden layer. Input the neurons as a comma-separated list. \
+            For example: 10, 5, 10."
+        self.neurons_label.setToolTip(neurons_tip)
+        self.neurons.setToolTip(neurons_tip)
+        
+        output_neurons_tip = "Number of neurons in the output layer."
+        self.output_neurons_label.setToolTip(output_neurons_tip)
+        self.output_neurons.setToolTip(output_neurons_tip)
 
-        # n_estimators_tip = "The number of trees in the forest."
-        # self.n_estimators.setToolTip(n_estimators_tip)
-        # self.n_estimators_label.setToolTip(n_estimators_tip)
+        activation_tip = "Activation function used in each hidden layer."
+        self.activation_label.setToolTip(activation_tip)
+        self.activation.setToolTip(activation_tip)
 
-        # criterion_tip = "" # TODO
-        # self.criterion.setToolTip(criterion_tip)
-        # self.criterion_label.setToolTip(criterion_tip)
+        last_activation_tip = "Activation function used in the output layer."
+        self.last_activation_label.setToolTip(last_activation_tip)
+        self.last_activation.setToolTip(last_activation_tip)
 
-        # max_depth_tip = (
-        #     "The maximum depth of the tree. If None, nodes are expanded until all leaves are pure or"
-        #     " until all leaves contain less than min_samples_split samples."
-        # )
-        # self.max_depth.setToolTip(max_depth_tip)
-        # self.max_depth_label.setToolTip(max_depth_tip)
+        epochs_tip = "Number of epochs to train the model."
+        self.epochs_label.setToolTip(epochs_tip)
+        self.epochs.setToolTip(epochs_tip)
 
+        batch_size_tip = "Number of samples per gradient update."
+        self.batch_size_label.setToolTip(batch_size_tip)
+        self.batch_size.setToolTip(batch_size_tip)
 
+        optimizer_tip = "Optimizer to be used."
+        self.optimizer_label.setToolTip(optimizer_tip)
+        self.optimizer.setToolTip(optimizer_tip)
+
+        learning_rate_tip = "Learning rate to be used in training."
+        self.learning_rate_label.setToolTip(learning_rate_tip)
+        self.learning_rate.setToolTip(learning_rate_tip)
+
+        loss_function_tip = "Loss function to be used."
+        self.loss_function_label.setToolTip(loss_function_tip)
+        self.loss_function.setToolTip(loss_function_tip)
+
+        dropout_rate_tip = "Fraction of the input units to drop."
+        self.dropout_rate_label.setToolTip(dropout_rate_tip)
+        self.dropout_rate.setToolTip(dropout_rate_tip)
+
+        early_stopping_tip = "Whether to use early stopping in training."
+        self.early_stopping_label.setToolTip(early_stopping_tip)
+        self.early_stopping.setToolTip(early_stopping_tip)
+
+        es_patience_tip = "Number of epochs with no improvement after which training will be stopped."
+        self.es_patience_label.setToolTip(es_patience_tip)
+        self.es_patience.setToolTip(es_patience_tip)
