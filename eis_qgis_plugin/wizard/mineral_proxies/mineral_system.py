e41ac34de5515b4a8d3faf39fdb57fe7bf0699da
Niko Aarnio <niko.aarnio@protonmail.com>
2024-09-26 14:38:05 +0300

    refactor(wizard-proxies): - Separate JSONs for each mineral system, change JSON schema - Big refactor to mineral systems and mineral proxies data structures and UI code

---
+++ eis_qgis_plugin/wizard/mineral_proxies/mineral_system.py
@@ -0,0 +1,99 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional, Sequence
+
+
+class ProxyWorkflows(Enum):
+     RED = 1
+     BLUE = 2
+     GREEN = 3
+
+
+@dataclass
+class ProxyImportance:
+    description: str
+    value: int
+    color_coding: str
+    tooltip_text: str
+
+    @classmethod
+    def from_description(self, text: str):
+        if text == "High":
+            return ProxyImportance(text, 1, "red", "Importance: High")
+        elif text == "Moderate":
+            return ProxyImportance(text, 2, "orange", "Importance: Moderate")
+        elif text == "Low":
+            return ProxyImportance(text, 3, "green", "Importance: Low")
+        elif text == "-":
+            return ProxyImportance(text, 4, "black", "Importance: Undefined")
+        else:
+            raise Exception(
+                f"Unrecognized proxy importance definition found in JSON: {text}. Importance should be either \
+                    'High', 'Moderate', 'Low' or '-'."
+            )
+
+
+@dataclass
+class MineralProxy:
+    name: str
+    custom: bool
+    mineral_system_component: str
+    category: str
+    workflow: int
+    regional_scale_importance: ProxyImportance
+    camp_scale_importance: ProxyImportance
+    deposit_scale_importance: ProxyImportance
+
+    @classmethod
+    def new(self, source_dict: dict, mineral_system_component: str):
+        importances = source_dict.pop("importance")
+        custom = False
+        return MineralProxy(
+            custom=custom,
+            mineral_system_component=mineral_system_component,
+            regional_scale_importance=ProxyImportance.from_description(importances["regional"]),
+            camp_scale_importance=ProxyImportance.from_description(importances["camp"]),
+            deposit_scale_importance=ProxyImportance.from_description(importances["deposit"]),
+            **source_dict
+        )
+
+    def importance_from_scale(self, scale: str) -> Optional[ProxyImportance]:
+        if scale == "regional":
+            return self.regional_scale_importance
+        elif scale == "camp":
+            return self.camp_scale_importance
+        elif scale == "deposit":
+            return self.deposit_scale_importance
+        else:
+            print("Unrecognized scale!")
+            return None  # NOTE: Check
+
+
+@dataclass
+class MineralSystem:
+    name: str
+    custom: bool
+    proxies: Sequence[MineralProxy]
+
+    @classmethod
+    def new(
+        self,
+        source_dict: dict,
+        components: Sequence[str] = ["source", "pathway", "depositional", "mineralisation"]
+    ):
+        proxies = []
+        try:
+            components_dict = source_dict.pop("mineral_system_components")
+        except KeyError:
+            print(f"Mineral system initialization failed as 'mineral_system_components' were not \
+                found for source dict {source_dict}")
+        for component in components:
+            proxy_details_for_component = components_dict.get(component)
+            for proxy_details in proxy_details_for_component:
+                proxy = MineralProxy.new(proxy_details, component)
+                proxies.append(proxy)
+
+        return MineralSystem(proxies=proxies, **source_dict)
+
+    def to_json():
+        pass
--- eis_qgis_plugin/wizard/wizard_proxies.py
+++ eis_qgis_plugin/wizard/wizard_proxies.py
@@ -1,4 +1,6 @@
-from typing import Optional
+import json
+import os
+from typing import Optional, Sequence
 
 from qgis.PyQt.QtWidgets import (
     QStackedWidget,
@@ -6,6 +8,7 @@ from qgis.PyQt.QtWidgets import (
     QWidget,
 )
 
+from eis_qgis_plugin.wizard.mineral_proxies.mineral_system import MineralProxy, MineralSystem
 from eis_qgis_plugin.wizard.mineral_proxies.proxy_view import EISWizardProxyView
 from eis_qgis_plugin.wizard.mineral_proxies.workflows.distance_to_anomaly import EISWizardProxyDistanceToAnomaly
 from eis_qgis_plugin.wizard.mineral_proxies.workflows.distance_to_features import EISWizardProxyDistanceToFeatures
@@ -21,10 +24,15 @@ class EISWizardProxies(QWidget):
         4: [EISWizardProxyInterpolate, EISWizardProxyDistanceToAnomaly]
     }
 
+    MINERAL_SYSTEMS_DIR = os.path.join(os.path.dirname(__file__), "mineral_proxies/mineral_system_libraries")
+
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
 
-        self.proxy_view = EISWizardProxyView(proxy_manager=self)
+        self.mineral_systems = self.initialize_mineral_systems()
+
+        # Init widgets
+        self.proxy_view = EISWizardProxyView(proxy_manager=self, mineral_systems=self.mineral_systems)
         self.proxy_pages = QStackedWidget(self)
         self.proxy_pages.addWidget(self.proxy_view)
 
@@ -35,42 +43,58 @@ class EISWizardProxies(QWidget):
         self.active_proxy_name: Optional[str] = None
 
 
-    def enter_proxy_processing(
-        self,mineral_system: str,
-        category: str,
-        proxy_name: str,
-        workflow: int,
-        mineral_system_component: str
-    ):  
+    def initialize_mineral_systems(self) -> Sequence[MineralSystem]:
+        mineral_systems = []
+        # Find all JSON files in the folder dedicated to mineral system libraries
+        for file_name in os.listdir(self.MINERAL_SYSTEMS_DIR):
+            if file_name.endswith(".json"):
+                mineral_system_dict = self._read_mineral_system_json(file_name)
+                mineral_system = MineralSystem.new(mineral_system_dict)
+                mineral_systems.append(mineral_system)
+        return mineral_systems
+
+
+    def _read_mineral_system_json(self, file_name) -> dict:
+        fp = os.path.join(self.MINERAL_SYSTEMS_DIR, file_name)
+        with open(fp, "r") as file:
+            mineral_system_dict = json.loads(file.read())
+        return mineral_system_dict
+
+
+
+# ------------------------------
+
+
+    def enter_proxy_processing(self, mineral_system_name: str, proxy: MineralProxy):  
         # Cleanup if changing proxy
-        if self.active_proxy_name and self.active_proxy_name != proxy_name:
+        if self.active_proxy_name and self.active_proxy_name != proxy.name:
             self.delete_proxy_processing_pages()
 
-        workflow_classes = self.WORKFLOW_MAP[workflow]
+        workflow_classes = self.WORKFLOW_MAP[proxy.workflow]
         if isinstance(workflow_classes, list):
             last_i = len(workflow_classes) - 1
             for i, cls in enumerate(workflow_classes):
                 processing_page = cls(
                     proxy_manager=self,
-                    mineral_system=mineral_system,
-                    category=category,
-                    proxy_name=proxy_name,
-                    mineral_system_component=mineral_system_component,
+                    mineral_system=mineral_system_name,
+                    category=proxy.category,
+                    proxy_name=proxy.name,
+                    mineral_system_component=proxy.mineral_system_component,
                     process_type="multi_step" if i != last_i else "multi_step_final"
                 )
                 self.proxy_pages.addWidget(processing_page)
         else:
-            processing_page = self.WORKFLOW_MAP[workflow](
+            processing_page = self.WORKFLOW_MAP[proxy.workflow](
                 proxy_manager=self,
-                mineral_system=mineral_system,
-                category=category,
-                proxy_name=proxy_name,
-                mineral_system_component=mineral_system_component,
+                mineral_system=mineral_system_name,
+                category=proxy.category,
+                proxy_name=proxy.name,
+                mineral_system_component=proxy.mineral_system_component,
                 process_type="single_step"
             )
             self.proxy_pages.addWidget(processing_page)
 
-        self.active_proxy_name = proxy_name
+        self.active_proxy_name = proxy.name
         self.proxy_pages.setCurrentIndex(1)     
 
 
