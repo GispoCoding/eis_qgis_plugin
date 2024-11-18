8c66bbe6e7cd2dfe246e6678f14f9a436dc9daaf
msorvoja <mika@gispo.fi>
2024-11-18 11:52:27 +0200

    refactor(coda): Add columns selection parameter

--- eis_qgis_plugin/eis_processing/algorithms/transformations_coda/clr_transform.py
+++ eis_qgis_plugin/eis_processing/algorithms/transformations_coda/clr_transform.py
@@ -1,5 +1,6 @@
 from qgis.core import (
     QgsProcessingParameterFeatureSource,
+    QgsProcessingParameterField,
     QgsProcessingParameterVectorDestination,
 )
 
@@ -17,7 +18,7 @@ class EISClrTransform(EISProcessingAlgorithm):
         self._short_help_string = "Perform a centered logratio transformation on the data."
 
     def initAlgorithm(self, config=None):
-        self.alg_parameters = ["input_vector", "output_vector"]
+        self.alg_parameters = ["input_vector", "columns", "output_vector"]
 
         input_vector_param = QgsProcessingParameterFeatureSource(
             name=self.alg_parameters[0], description="Input vector"
@@ -25,8 +26,18 @@ class EISClrTransform(EISProcessingAlgorithm):
         input_vector_param.setHelp("Input vector with compositional data.")
         self.addParameter(input_vector_param)
 
-        output_vector_param = QgsProcessingParameterVectorDestination(
+        columns_param = QgsProcessingParameterField(
             name=self.alg_parameters[1],
+            description="Columns",
+            parentLayerParameterName=self.alg_parameters[0],
+            optional=True,
+            allowMultiple=True
+        )
+        columns_param.setHelp("The names of the columns to be transformed.")
+        self.addParameter(columns_param)
+
+        output_vector_param = QgsProcessingParameterVectorDestination(
+            name=self.alg_parameters[2],
             description="Output vector",
         )
         output_vector_param.setHelp("Output vector with the CLR transformed data.")
