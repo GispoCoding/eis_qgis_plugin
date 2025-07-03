8f3aec2aaecd45e647c59d201428d2d48fec6c9a
Niko Aarnio <niko.aarnio@protonmail.com>
2025-07-03 08:54:05 +0300

    chore: Bump to version 1.0.6

--- eis_qgis_plugin/environment/eis_toolkit_invoker.py
+++ eis_qgis_plugin/environment/eis_toolkit_invoker.py
@@ -14,7 +14,7 @@ from eis_qgis_plugin.utils.model_feedback import EISProcessingFeedback
 from eis_qgis_plugin.utils.settings_manager import EISSettingsManager
 
 DEBUG = True
-REQUIRED_EIS_TOOLKIT_VERSION = "1.1.5"
+REQUIRED_EIS_TOOLKIT_VERSION = "1.1.6"
 
 logger = logging.getLogger(__name__)
 
